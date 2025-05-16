19734ad5aedf538f602ddd122b97843831c0affc
Sagar1643 <sagar.r@s.amity.edu>
2025-05-16 20:52:29 +0530

    modified the exusting file

--- add.c
+++ add.c
@@ -9,3 +9,12 @@ int main() {
 
     printf("Enter second number: ");
     scanf("%d", &num2);
+
+    // Calculate the sum
+    sum = num1 + num2;
+
+    // Display the result
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
