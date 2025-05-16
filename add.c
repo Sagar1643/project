9872e5675f0dd0c8cee856f056ef4d281b412cc8
Sagar1643 <sagar.r@s.amity.edu>
2025-05-16 20:50:50 +0530

    add of two number

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Ask the user to enter two numbers
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
